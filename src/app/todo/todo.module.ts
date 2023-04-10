b9c086ee5e73243687fe5917f4dec8594f86e63a
Heasterian <maciek.cienkowski2@gmail.com>
2023-04-10 13:13:07 +0200

    Minor fixes

--- src/app/todo/todo.module.ts
+++ src/app/todo/todo.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
-import { TodoComponent } from './component/todo.component';
+import { TodoComponent } from './components/todo.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { DragDropModule } from '@angular/cdk/drag-drop';
